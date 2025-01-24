32cf93730f7862f30740288cfd53a7d583283205
Unknown-zzz <unknown.zzz.2909@gmail.com>
2025-01-24 09:53:45 -0400

    Base de datos

---
+++ db_proyecto (1).sql
@@ -0,0 +1,314 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-01-2025 a las 14:53:28
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_proyecto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id_categoria`, `nombre`) VALUES
+(1, 'Tecnologia'),
+(2, 'Cultura'),
+(3, 'Social'),
+(4, 'Comunicacion');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id_comentario` int(11) NOT NULL,
+  `id_noticia` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_comentario_padre` int(11) DEFAULT NULL,
+  `contenido` text NOT NULL,
+  `fecha_comen` timestamp NOT NULL DEFAULT current_timestamp(),
+  `estado` enum('activo','inactivo') DEFAULT 'activo'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `id_noticias` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `contenido` text NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  `categoria_id` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `destacado` tinyint(1) DEFAULT 0,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `noticias`
+--
+
+INSERT INTO `noticias` (`id_noticias`, `titulo`, `contenido`, `imagen`, `categoria_id`, `id_usuario`, `destacado`, `fecha_creacion`) VALUES
+(1, 'Prueba 777', 'Hola', 'uploads/678e51399339e_images.jpeg', 1, 2, 1, '2025-01-20 13:35:53'),
+(3, 'Noticia 1', 'Noticia', 'uploads/67924c81a38b8_logo-universidad-privada-domingo-savio (1).png', 1, 2, 1, '2025-01-23 14:04:49'),
+(4, 'Noticia 2', 'Noticia', 'uploads/67924c8ec0f62_logo-universidad-privada-domingo-savio (1).png', 3, 2, 1, '2025-01-23 14:05:02'),
+(5, 'Noticia 3', 'Noticia', 'uploads/67924c9ac466d_Imagen de WhatsApp 2025-01-09 a las 21.24.49_eecbc436.jpg', 3, 2, 1, '2025-01-23 14:05:14'),
+(6, 'Noticia 4', 'Noticia', 'uploads/67924ca3e8ee4_Imagen de WhatsApp 2025-01-09 a las 21.18.33_64725603.jpg', 2, 2, 1, '2025-01-23 14:05:23'),
+(7, 'Noticia 5', 'Noticia', 'uploads/67924cb1c6f6b_Imagen de WhatsApp 2025-01-09 a las 21.24.49_eecbc436.jpg', 3, 2, 0, '2025-01-23 14:05:37'),
+(8, 'Noticia 6', 'Noticia', 'uploads/67924cd34a61d_logo-universidad-privada-domingo-savio (1).png', 2, 2, 1, '2025-01-23 14:06:11');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `id_permisos` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `permisos`
+--
+
+INSERT INTO `permisos` (`id_permisos`, `nombre`, `descripcion`) VALUES
+(1, 'ver_dashboard', 'Acceso al dashboard principal'),
+(2, 'gestionar_usuarios', 'Permiso para gestionar usuarios'),
+(3, 'crear_noticia', 'Permiso para crear noticias');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `nombre`, `descripcion`) VALUES
+(1, 'Administrador', 'Administrador'),
+(2, 'Lector', 'Lector.'),
+(3, 'Visitante', 'Visitante');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles_permisos`
+--
+
+CREATE TABLE `roles_permisos` (
+  `id_rol` int(11) NOT NULL,
+  `id_permisos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles_permisos`
+--
+
+INSERT INTO `roles_permisos` (`id_rol`, `id_permisos`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `genero` enum('M','F','O') NOT NULL,
+  `direccion` text DEFAULT NULL,
+  `nacionalidad` varchar(100) DEFAULT NULL,
+  `num_telefono` varchar(15) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `rol_id` int(11) DEFAULT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `email`, `password`, `genero`, `direccion`, `nacionalidad`, `num_telefono`, `fecha_nacimiento`, `rol_id`, `fecha_registro`) VALUES
+(1, 'Usuario', 'j29s09c03@gmail.com', '$2y$10$95XTctCsLsSKDPhsy/f1Rem6doJsJTcZKVQO2HmMrwSQFcgYH4Kqy', 'M', 'no', 'Bolivia', '1234567891', '2003-09-29', 2, '2025-01-16 14:21:58'),
+(2, 'Administrador', 'jonathanscm290903@gmail.com', '$2y$10$95XTctCsLsSKDPhsy/f1Rem6doJsJTcZKVQO2HmMrwSQFcgYH4Kqy', '', 'Mi casa', 'Bolivia', '1234567891', '2003-09-29', 1, '2025-01-16 14:21:58'),
+(4, 'PruebaUsuario', 'user@gmail.com', '$2y$10$ler05ywxdzxSBsoVcXpzmOhX.ugIS9oYtxgpQIwGq3aH5VNti3ef6', '', 'Mi casa', 'Boliviano', '69160031', '2003-09-29', 2, '2025-01-23 13:04:21'),
+(5, 'Pedrito', 'pedro@gmail.com', '$2y$10$1OX6MUhUqZME5wqqA4wv0OFVSYNqNrUx/wQnHAaKfmH5hdiCmAKEq', '', 'Mi casa', 'Peruano', '69160034', '1236-12-12', 2, '2025-01-24 13:20:48');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `id_noticia` (`id_noticia`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_comentario_padre` (`id_comentario_padre`);
+
+--
+-- Indices de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`id_noticias`),
+  ADD KEY `categoria_id` (`categoria_id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD PRIMARY KEY (`id_permisos`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `roles_permisos`
+--
+ALTER TABLE `roles_permisos`
+  ADD PRIMARY KEY (`id_rol`,`id_permisos`),
+  ADD KEY `id_permisos` (`id_permisos`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `rol_id` (`rol_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id_noticias` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  MODIFY `id_permisos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`id_noticia`) REFERENCES `noticias` (`id_noticias`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_3` FOREIGN KEY (`id_comentario_padre`) REFERENCES `comentarios` (`id_comentario`) ON DELETE SET NULL;
+
+--
+-- Filtros para la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  ADD CONSTRAINT `noticias_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id_categoria`) ON DELETE SET NULL,
+  ADD CONSTRAINT `noticias_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `roles_permisos`
+--
+ALTER TABLE `roles_permisos`
+  ADD CONSTRAINT `roles_permisos_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`) ON DELETE CASCADE,
+  ADD CONSTRAINT `roles_permisos_ibfk_2` FOREIGN KEY (`id_permisos`) REFERENCES `permisos` (`id_permisos`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id_rol`) ON DELETE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
